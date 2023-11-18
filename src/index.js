a03d2cbd45a321dab98ec8398e1f714e5ec79e53
dev <devpalwar06@gmail.com>
2023-11-18 16:32:33 +0530

    Trying to deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
-const connectDatabase = require("../Database/config.js");
-const graphqlInit = require("../GraphQl/server");
+const connectDatabase = require("../src/Database/Config");
+const graphqlInit = require("../src/GraphQl/server");
 
 // initializes enviorment variables
 dotenv.config();
